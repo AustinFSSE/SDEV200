9e443cc296ea8cb4557d50fa3e40f3d7cf0ee441
AustinFSSE <108036029+AustinFSSE@users.noreply.github.com>
2024-09-26 22:38:44 -0400

    Delete FinalProject directory

--- FinalProject/src/main/java/com/github/austinfsse/sdev200/finalproject/Controllers/Clients/ClientController.java
+++
@@ -1,8 +0,0 @@
-package com.github.austinfsse.sdev200.finalproject.Controllers.Clients;
-
-import javafx.scene.control.Button;
-import javafx.scene.control.TextField;
-
-public class ClientController {
-
-}
